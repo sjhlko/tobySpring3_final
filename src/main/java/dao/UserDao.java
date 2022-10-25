13ac30dfb41fd4f928061609646730d99b3b317f
서정희 <sjhlko@naver.com>
2022-10-25 11:41:06 +0900

    connection 분리 (interface)

---
+++ src/main/java/dao/UserDao.java
@@ -0,0 +1,68 @@
+package dao;
+
+import domain.User;
+
+import java.sql.*;
+import java.util.Map;
+
+public class UserDao {
+    private ConnectionMaker connectionMaker;
+    public  UserDao(){
+        this.connectionMaker = new AwsConnectionMaker();
+    }
+    public void add(User user) {
+
+        try {
+
+            // DB접속 (ex sql workbeanch실행)
+            Connection c = connectionMaker.makeConnection();
+
+            // Query문 작성
+            PreparedStatement pstmt = c.prepareStatement("INSERT INTO users(id, name, password) VALUES(?,?,?);");
+            pstmt.setString(1, user.getId());
+            pstmt.setString(2, user.getName());
+            pstmt.setString(3, user.getPassword());
+
+            // Query문 실행
+            pstmt.executeUpdate();
+
+            pstmt.close();
+            c.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public User findById(String id) {
+        try {
+            // DB접속 (ex sql workbeanch실행)
+            Connection c = connectionMaker.makeConnection();
+
+            // Query문 작성
+            PreparedStatement pstmt = c.prepareStatement("SELECT * FROM users WHERE id = ?");
+            pstmt.setString(1, id);
+
+            // Query문 실행
+            ResultSet rs = pstmt.executeQuery();
+            rs.next();
+            User user = new User(rs.getString("id"), rs.getString("name"),
+                    rs.getString("password"));
+
+            rs.close();
+            pstmt.close();
+            c.close();
+
+            return user;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        UserDao userDao = new UserDao();
+        User user = userDao.findById("1");
+        System.out.println(user.getName());
+    }
+}
