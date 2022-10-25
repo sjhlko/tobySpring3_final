e57dbcca24c570829f069c6c8bbf263e9bc2f3e9
서정희 <sjhlko@naver.com>
2022-10-25 13:35:52 +0900

    Spring-boot-starter-jdbc, spring-boot-starter-test 추가 Spring 도입 – 98p Test Code를 ApplicationContext사용하게 수정

--- src/main/java/dao/UserDaoFactory.java
+++ src/main/java/dao/UserDaoFactory.java
@@ -1,7 +1,12 @@
 package dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class UserDaoFactory {
+
+    @Bean
     public UserDao awsUserDao(){
         AwsConnectionMaker awsConnectionMaker = new AwsConnectionMaker();
         UserDao userDao = new UserDao(awsConnectionMaker);
--- src/test/java/dao/UserDaoTest.java
+++ src/test/java/dao/UserDaoTest.java
@@ -3,17 +3,28 @@ package dao;
 import domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = UserDaoFactory.class)
 class UserDaoTest {
+
+    @Autowired
+    ApplicationContext context;
     @Test
     void addAndSelect() throws SQLException, ClassNotFoundException {
-        UserDao userDao = new UserDaoFactory().awsUserDao();
-        User user = new User ("345","hi","1234");
+        UserDao userDao = context.getBean("awsUserDao",UserDao.class);
+        User user = new User ("3445","hi","1234");
         userDao.add(user);
-        User selectedUser = userDao.findById("345");
+        User selectedUser = userDao.findById("3445");
         Assertions.assertEquals("hi",selectedUser.getName());
     }
 
